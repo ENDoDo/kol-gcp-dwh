7e09144c51dfa0e23d39eb3457109a90c430d40c
ENDoDo <t.endo0521@gmail.com>
2026-04-24 23:19:48 +0900

    feat: race_kekka_timeテーブルの作成とワークフローへの追加

--- terraform/dataform.tf
+++ terraform/dataform.tf
@@ -185,6 +185,11 @@ resource "google_dataform_repository_workflow_config" "workflow_stg" {
       schema   = var.stg_schema
       name     = "race_kekka_keika"
     }
+    included_targets {
+      database = var.project_id
+      schema   = var.stg_schema
+      name     = "race_kekka_time"
+    }
     service_account = google_service_account.dataform.email
   }
 
@@ -279,6 +284,11 @@ resource "google_dataform_repository_workflow_config" "workflow_prd" {
       database = var.project_id
       schema   = var.prd_schema
       name     = "race_kekka_keika"
+    }
+    included_targets {
+      database = var.project_id
+      schema   = var.prd_schema
+      name     = "race_kekka_time"
     }
     service_account = google_service_account.dataform.email
   }
